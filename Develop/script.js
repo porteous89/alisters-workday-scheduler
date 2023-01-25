fa8b1e409aed25d57f24468530513a9b4eabb3da
Alister Porteous <alisterporteous@hotmail.com>
2023-01-25 14:19:03 -0500

    used Day.js to get current date, formatted it to display

--- Develop/script.js
+++ Develop/script.js
@@ -20,4 +20,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  const currentDay = dayjs().format("dddd, MMMM D, YYYY");
+document.getElementById("currentDay").innerHTML = currentDay;
 });
